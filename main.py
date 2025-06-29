90dd40f89050ca6725dfc8715a5e21bb6d44a09f
Rafael Conrado <rafaelconrado@MacBook-Air-de-admin.local>
2025-06-29 13:48:18 -0300

    logo novo tamaho

--- main.py
+++ main.py
@@ -45,6 +45,8 @@ app.mount(
 # Middleware para validar o header X-Broker-Api-Version
 @app.middleware("http")
 async def validar_header_x_broker_api_version(request: Request, call_next):
+    logger.info(f"Middleware request path: {request.url.path}")
+
     # rotas que não precisam do header (status ou docs genéricos)
     rotas_liberadas = ["/", "/docs", "/openapi.json", "/images", f"{settings.ROOT_PATH}/images"]
 
